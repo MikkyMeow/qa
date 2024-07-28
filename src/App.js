c8bbd3da5061dbe503b36d530e71501560e33339
Artur Akhmadullin <artur.a@itechpsp.com>
2024-07-28 17:10:26 +0300

    answers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { themes } from "./content";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {themes.map((theme) => (
+        <li>
+          <p>{theme.header}</p>
+          <ul>
+            {theme.questions.map(({ question, answer }) => (
+              <li>
+                <p>{question}</p>
+                <p>{answer}</p>
+              </li>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
   );
 }
 
